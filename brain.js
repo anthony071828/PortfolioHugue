81bdc0b9d716c25a2815ba697fb84b14baea4974
Anthony David Josh Hugue <145982415+anthony071828@users.noreply.github.com>
2025-08-30 13:33:54 +0800

    Add files via upload

---
+++ brain.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navbar links
+document.querySelectorAll("a.nav-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
